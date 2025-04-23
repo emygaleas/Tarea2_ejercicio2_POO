9fa87c7b795af337a170bd7ecdd03deafd715a61
Emy <emily.galeas@epn.edu.ec>
2025-04-23 18:35:45 -0500

    Ejercicio 2, Sistema de Gestión Empleados

---
+++ src/Empleado.java
@@ -0,0 +1,25 @@
+public class Empleado {
+    //nombre, posición en el campo, edad y número de camiseta
+    private String nombre;
+    private String cargo;
+    private Double salario;
+    private Integer dia;
+    private Integer mes;
+    private Integer anio;
+
+    public Empleado(String nombre, String cargo, Double salario, Integer dia, Integer mes, Integer anio){
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+    public void mostrar_informacion(){
+        System.out.println("Nombre del Empleado: "+nombre);
+        System.out.println("Cargo: "+cargo);
+        System.out.println("Salario: "+salario);
+        System.out.println("Fecha de ingreso a la empresa: "+dia+"/"+mes+"/"+anio);
+    }
+}
